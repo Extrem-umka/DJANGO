026fb8469f162d7ed6def4fc56a9ddab90b4c441
Extrem_umka <zhulianev@gmail.com>
2023-01-26 17:59:01 +0600

    Task 2.2_mod

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -6,7 +6,7 @@ from articles.models import Article
 def articles_list(request):
     template = 'articles/news.html'
     articles = Article.objects.all()
-    context = {'articles': articles}
+    context = {'object_list': articles}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
